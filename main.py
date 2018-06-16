11680326c349977bdc55ec78024551f47806bfe2
xedre <x@xed.re>
2018-06-16 19:58:38 +0100

    added nearclicked and begun mine placing
    Signed-off-by: xedre <x@xed.re>

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import random
-def generate(clicked, X, Y):
+
+
+def generate(clicked, X, Y, mines):
     map = []
     for x in range(X):
         map.append([])
         for y in range(Y):
             map[x].append(1)
-    for j in range(10):
-        mines = random.randrange
 
+    nearclicked = []
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if X > clicked[0 + i] >= 0 and Y > clicked[1 + j] >= 0:
+                nearclicked.append((clicked[0 + i], clicked[1 + j]))
 
-
-
-
-
+    for mine in range(mines):
+        while True:
+            x = random.randrange(X)
+            y = random.randrange(Y)
 
 
 if __name__ == "__main__":
