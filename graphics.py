b2e4658ec84c8c6cb151e9931496cc7460f6cfaf
xedre <x@xed.re>
2018-06-16 19:36:26 +0100

    changed caption to "Minesweeper"
    Signed-off-by: xedre <x@xed.re>

--- graphics.py
+++ graphics.py
@@ -5,7 +5,7 @@ def start(screenSize):
     pygame.init()
     clock = pygame.time.Clock()
     gameDisplay = pygame.display.set_mode(screenSize)
-    pygame.display.set_caption("food")
+    pygame.display.set_caption("Minesweeper")
     pygame.display.update()
     font = pygame.font.SysFont('arial', 25)
     return gameDisplay, clock, font
