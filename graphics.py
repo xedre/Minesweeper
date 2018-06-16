af17b6c515f561467ab6f3a930db6b9742b2d30a
xedre <x@xed.re>
2018-06-16 19:35:51 +0100

    added pygame startup and shutdown
    Signed-off-by: xedre <x@xed.re>

---
+++ graphics.py
@@ -0,0 +1,15 @@
+import pygame
+
+
+def start(screenSize):
+    pygame.init()
+    clock = pygame.time.Clock()
+    gameDisplay = pygame.display.set_mode(screenSize)
+    pygame.display.set_caption("food")
+    pygame.display.update()
+    font = pygame.font.SysFont('arial', 25)
+    return gameDisplay, clock, font
+
+def end():
+    pygame.quit()
+    exit()
