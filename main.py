96f7f853f0c8db648a82d5a3c0741aabb9599edb
xedre <x@xed.re>
2018-06-17 10:30:16 +0100

    fixed tuple error
    Signed-off-by: xedre <x@xed.re>

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ def generate(clicked, X, Y, mines):
     nearclicked = []
     for i in range(-1, 2):
         for j in range(-1, 2):
-            if X > clicked[0 + i] >= 0 and Y > clicked[1 + j] >= 0:
-                nearclicked.append((clicked[0 + i], clicked[1 + j]))
+            if X > clicked[0] + i >= 0 and Y > clicked[1] + j>= 0:
+                nearclicked.append((clicked[0] + i, clicked[1] + j))
 
     for mine in range(mines):
         while True:
