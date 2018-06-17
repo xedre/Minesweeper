9e4feae1aff2cdf3280c717f399df921a0dc2b7f
xedre <x@xed.re>
2018-06-17 12:49:47 +0100

    PEP8
    Signed-off-by: xedre <x@xed.re>

--- graphics.py
+++ graphics.py
@@ -6,11 +6,11 @@ black = (50, 50, 50)
 def start(screenSize):
     pygame.init()
     clock = pygame.time.Clock()
-    gameDisplay = pygame.display.set_mode(screenSize)
+    display = pygame.display.set_mode(screenSize)
     pygame.display.set_caption("Minesweeper")
     pygame.display.update()
     font = pygame.font.SysFont('arial', 25)
-    return gameDisplay, clock, font
+    return display, clock, font
 
 
 def end():
